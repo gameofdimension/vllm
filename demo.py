c8556cf1394a9e32b6ac0b311b0b409315f8f4b3
felixdae <felixdae@gmail.com>
2023-11-07 12:28:15 +0800

    gc

--- demo.py
+++ demo.py
@@ -1,34 +1,26 @@
 import torch
 
 
-def run_llm():
+def run_llm(prompt):
     from vllm import LLM, SamplingParams
     llm = LLM(model="THUDM/chatglm3-6b", trust_remote_code=True, max_model_len=8192)
-    prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
-        "希望这篇文章能",
-        "给六岁小朋友解释一下万有引",
-    ]
     sampling_params = SamplingParams(temperature=0.0, max_tokens=1)
-    outputs = llm.generate(prompts, sampling_params)
+    outputs = llm.generate(prompt, sampling_params)
 
     # Print the outputs.
     for output in outputs:
-        prompt = output.prompt
+        prom = output.prompt
         generated_text = output.outputs[0].text
-        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+        print(f"Prompt: {prom!r}, Generated text: {generated_text!r}")
     del llm
 
 
-def run_transformer():
+def run_transformer(prompt):
     from transformers import AutoTokenizer, AutoModel
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
     model = AutoModel.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True, torch_dtype=torch.float32).cuda()
     model = model.eval()
-    tko = tokenizer("给六岁小朋友解释一下万有引")
+    tko = tokenizer(prompt)
     input_ids = torch.LongTensor([tko.input_ids]).to('cuda')
 
     outputs = model(input_ids, output_hidden_states=True)
@@ -37,14 +29,11 @@ def run_transformer():
         hidden_state = outputs.hidden_states[i]
         hidden_state = hidden_state.permute(1, 0, 2)
         print(f"gold {i}", hidden_state[..., :5])
-
-    outputs = torch.argmax(outputs.logits, dim=-1).tolist()[0]
-    print(outputs)
-    response = tokenizer.decode(outputs)
     del model
 
 
 if __name__ == '__main__':
     # check_rope()
-    run_llm()
-    run_transformer()
+    prompt = "给六岁小朋友解释一下万有引"
+    run_llm(prompt)
+    run_transformer(prompt)
